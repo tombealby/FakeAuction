3150d513872b673577c6b3a15449b4d747b42839
tombealby <tom.bealby67@gmail.com>
2023-09-09 16:43:33 +0100

    Initial maven project setup.

---
+++ src/main/java/bealby/tom/FakeAuction/FakeAuctionApplication.java
@@ -0,0 +1,13 @@
+package bealby.tom.FakeAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bealby/tom/FakeAuction/FakeAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package bealby.tom.FakeAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
