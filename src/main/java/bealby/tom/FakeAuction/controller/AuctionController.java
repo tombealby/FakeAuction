a632bc7382770aaa2f65a089e9d7be4c1d4e65c9
tombealby <tom.bealby67@gmail.com>
2023-09-09 17:22:43 +0100

    Method to close auction sends notification to sniper client.

--- src/main/java/bealby/tom/FakeAuction/controller/AuctionController.java
+++ src/main/java/bealby/tom/FakeAuction/controller/AuctionController.java
@@ -3,11 +3,14 @@ package bealby.tom.FakeAuction.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AuctionController {
 	private String status = "Open";
-	
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@RequestMapping("/receiveJoinRequest")
 	public ResponseEntity<String> receiveJoinRequest() {
 		System.out.println("Received a request to join the auction");
@@ -16,15 +19,19 @@ public class AuctionController {
 	
 	@RequestMapping("/closeAuction")
 	public ResponseEntity<String> closeAuction() {
-		// TODO notify clients that auction is closed
 		System.out.println("notify clients that auction is closed");
 		status = "Closed";
-		return ResponseEntity.ok("notified clients that auction is closed");
+		return sendRequest();
 	}
 	
 	@RequestMapping("/getStatus")
 	public ResponseEntity<String> getStatus() {
 		System.out.println("Received a request to show my status");
 		return ResponseEntity.ok("current status:" + status);
+	}
+
+	private ResponseEntity<String> sendRequest() throws HttpClientErrorException {
+	    final String url = "http://localhost:8092/receiveAuctionMessage";
+		return restTemplate.getForEntity(url, String.class);
 	}
 }
